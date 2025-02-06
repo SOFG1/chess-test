58efc4191929cc8762e71c071006c76f543bf024
Edaurd Kosyan <edkosyan2000@gmail.com>
2025-02-06 15:36:42 +0400

    add possbile moves switcher

--- src/utils/calculatePossibleMoves.ts
+++ src/utils/calculatePossibleMoves.ts
@@ -1,9 +1,47 @@
 import { CellType } from "@/types";
 
+export function calculatePossibleMoves(
+  table: CellType[],
+  figureIndex: number
+): number[] {
+  const figureType = table[figureIndex].type;
 
+  switch (figureType) {
+    case "pawn":
+      return pawnMoves(table, figureIndex);
+    case "knight":
+      return knightMoves(table, figureIndex);
+    case "bishop":
+      return bishopMoves(table, figureIndex);
+    case "rook":
+      return rookMoves(table, figureIndex);
+    case "queen":
+      return queenMoves(table, figureIndex);
+    case "king":
+      return kingMoves(table, figureIndex);
+  }
+}
 
+function pawnMoves(table: CellType[], figureIndex: number): number[] {
+  return [44, 36, 12];
+}
 
-export function calculatePossibleMoves(table: CellType[], figureIndex: number): number[] {
-    const figureType = table[figureIndex].type
-    return [44, 36, 12]
+function knightMoves(table: CellType[], figureIndex: number): number[] {
+  return [44, 36, 12];
+}
+
+function bishopMoves(table: CellType[], figureIndex: number): number[] {
+  return [44, 36, 12];
+}
+
+function rookMoves(table: CellType[], figureIndex: number): number[] {
+  return [44, 36, 12];
+}
+
+function queenMoves(table: CellType[], figureIndex: number): number[] {
+  return [44, 36, 12];
+}
+
+function kingMoves(table: CellType[], figureIndex: number): number[] {
+  return [44, 36, 12];
 }
