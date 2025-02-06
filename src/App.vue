c04914ddf5049fb2bfb9c52faebe2e1d66d9615f
Edaurd Kosyan <edkosyan2000@gmail.com>
2025-02-06 11:56:02 +0400

    add AccountComponent

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
 import GamePage from '@/pages/GamePage.vue';
-import HelloWorld from '@/components/HelloWorld.vue';
-
 
 </script>
 
 <template>
-  <div>
     <GamePage />
-  </div>
 </template>
 
 <style scoped></style>
--- src/pages/GamePage.vue
+++ src/pages/GamePage.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-
+import Player from "@/components/Player.vue";
 </script>
 
 <template>
-  <div>Game page</div>
+  <div class="wrapper">
+    <Player />
+    <Player />
+  </div>
 </template>
 
 <style scoped>
-
+.wrapper {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+}
 </style>
